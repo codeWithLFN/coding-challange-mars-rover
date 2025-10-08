7fd0aa419118c77419abc890dc96811eb0a35082
Lufuno Nemudzivhadi <lufuno.nemudzivhadi26@outlook.com>
2025-10-08 16:01:55 +0200

    first commit

---
+++ src/main/java/com/coding/challange/codingchallangemarsrover/CodingChallangeMarsRoverApplication.java
@@ -0,0 +1,13 @@
+package com.coding.challange.codingchallangemarsrover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingChallangeMarsRoverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodingChallangeMarsRoverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coding/challange/codingchallangemarsrover/CodingChallangeMarsRoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding.challange.codingchallangemarsrover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingChallangeMarsRoverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
